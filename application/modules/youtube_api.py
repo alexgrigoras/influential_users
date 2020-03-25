b7e8a5b8c5a07daf74170cd30edbc148c83d0685
alexgrigoras <alex_grigoras_10@yahoo.com>
2020-03-25 22:46:57 +0200

    Added method to write to json files, method for getting the video statistics and updated search_videos and get_videos methods

--- application/modules/youtube_api.py
+++ application/modules/youtube_api.py
@@ -1,5 +1,8 @@
 import csv
 import os
+import math
+import json
+import itertools
 from dotenv import load_dotenv
 
 from googleapiclient.errors import HttpError
@@ -18,12 +21,22 @@ YOUTUBE_API_VERSION = 'v3'
 class YoutubeAPI:
     def __init__(self):
         self.auth_service = self.__authentication_service()
+        self.channels = []
+        self.videos = []
+        self.comments = []
 
     @staticmethod
     def __authentication_service():
         return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
 
-    def get_video_comments(self, **kwargs):
+    @staticmethod
+    def __write_json_to_file(data, file_name):
+        json_data = json.dumps(data, indent=4)
+        f = open("../data/" + file_name + ".json", "w")
+        f.write(json_data)
+        f.close()
+
+    def __get_video_comments(self, **kwargs):
         comments = []
         try:
             results = self.auth_service.commentThreads().list(**kwargs).execute()
@@ -45,23 +58,40 @@ class YoutubeAPI:
 
         return comments
 
-    @staticmethod
-    def __write_to_csv(comments):
-        with open('../data/comments.csv', 'w') as comments_file:
-            comments_writer = csv.writer(comments_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            comments_writer.writerow(['Video ID', 'Title', 'Comment'])
-            for row in comments:
-                # convert the tuple to a list and write to the output file
-                comments_writer.writerow(list(row))
-
-    def get_videos(self, **kwargs):
+    def __get_video_details(self, **kwargs):
         final_results = []
+
+        results = self.auth_service.videos().list(**kwargs).execute()
+
+        while results:
+            for item in results['items']:
+                details = {
+                    'video_id': item['id'],
+                    'view_count': item['statistics']['viewCount'],
+                    'like_count': item['statistics']['likeCount'],
+                    'dislike_count': item['statistics']['dislikeCount'],
+                    'favorite_count': item['statistics']['favoriteCount'],
+                    'comment_count': item['statistics']['commentCount']
+                }
+
+                final_results.append(details)
+
+            # Check if another page exists
+            if 'nextPageToken' in results:
+                kwargs['pageToken'] = results['nextPageToken']
+                results = self.auth_service.videos().list(**kwargs).execute()
+            else:
+                break
+
+        return final_results
+
+    def __get_videos(self, nr_pages, **kwargs):
+        final_results = []
+        index = 0
+
         results = self.auth_service.search().list(**kwargs).execute()
 
-        index = 0
-        max_pages = 5
-
-        while results and index < max_pages:
+        while results and index < nr_pages:
             final_results.extend(results['items'])
 
             # Check if another page exists
@@ -74,25 +104,93 @@ class YoutubeAPI:
 
         return final_results
 
-    def search_videos_by_keyword(self, **kwargs):
-        print("Searching videos:")
-        results = self.get_videos(**kwargs)
-        final_result = []
+    def search_videos(self, **kwargs):
+        print("Searching videos by keyword [" + kwargs.get("k") + "]:")
+
+        search_keyword = ""
+
+        if "k" in kwargs.keys():
+            search_keyword = kwargs.get("k")
+        elif "l" in kwargs.keys():
+            search_location = kwargs.get("l")
+        else:
+            print("Invalid search parameters")
+            return
+
+        nr_pages = 1
+        max_results = 50
+        if "nr" in kwargs.keys():
+            nr_of_results = kwargs.get("nr")
+            if nr_of_results > 50:
+                nr_pages = math.ceil(nr_of_results / 50)
+            else:
+                max_results = nr_of_results
+
+        if "order" in kwargs.keys():
+            order = kwargs.get("order")
+        else:
+            order = "relevance"
+
+        results = self.__get_videos(nr_pages, q=search_keyword, part='id,snippet', eventType='completed', type='video',
+                                    maxResults=max_results, order=order)
+
+        videos_list = []
+
         for item in results:
             title = item['snippet']['title']
-            video_id = item['id']['videoId']
-            print(" > " + title)
-            comments = self.get_video_comments(part='snippet', videoId=video_id, textFormat='plainText', maxResults=100)
-            # make a tuple consisting of the video id, title, comment and add the result to
-            # the final list
-            final_result.extend([(video_id, title, comment) for comment in comments])
+            description = item['snippet']['description']
+            published_at = item['snippet']['publishedAt']
+            kind = item['id']['kind']
 
-        self.__write_to_csv(final_result)
+            if kind == 'youtube#channel':
+                print(" > Channel: " + title)
+
+                channel_id = item['id']['channelId']
+                self.channels.append({
+                    "id": channel_id,
+                    "title": title,
+                    "description": description,
+                    "published_at": published_at
+                })
+            elif kind == 'youtube#video':
+                print(" > Video: " + title)
+
+                video_id = item['id']['videoId']
+                channel_id = item['snippet']['channelId']
+
+                videos_list.append(video_id)
+
+                self.videos.append({
+                    "id": video_id,
+                    "channel_id": channel_id,
+                    "title": title,
+                    "description": description,
+                    "published_at": published_at,
+                })
+
+                comments = self.__get_video_comments(part='snippet,replies', videoId=video_id, textFormat='plainText',
+                                                     maxResults=100, order='time')
+                for comment in comments:
+                    self.comments.append({
+                        "video_id": video_id,
+                        "comment": comment
+                    })
+
+        videos_id_str = ""
+        for vid in videos_list:
+            videos_id_str += vid + ','
+        details = self.__get_video_details(part='statistics', id=videos_id_str, maxResults=max_results)
+
+        self.__write_json_to_file(self.channels, "channels")
+        self.__write_json_to_file(self.videos, "videos")
+        self.__write_json_to_file(self.comments, "comments")
 
 
 if __name__ == '__main__':
     keyword = input('Enter a keyword: ')
 
+    nr_results = 5
+
     api = YoutubeAPI()
 
-    api.search_videos_by_keyword(q=keyword, part='id,snippet', eventType='completed', type='video')
+    api.search_videos(k=keyword, nr=nr_results, order='relevance')
