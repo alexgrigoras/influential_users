20dce0027eb7b383a54c1200b5ce55322d9da54e
alexgrigoras <alex_grigoras_10@yahoo.com>
2020-03-29 21:11:37 +0300

    Updated to get the nr of videos from user and added the processing time of the execution of some methods

--- application/__main__.py
+++ application/__main__.py
@@ -1,13 +1,27 @@
+import time
 from application.modules.youtube_api import YoutubeAPI
 
 
 if __name__ == '__main__':
     keyword = input('Enter a keyword: ')
-    nr_videos = input('Enter the number of videos: ')
+    nr_videos_str = input('Enter the number of videos: ')
+    nr_videos = 0
+
+    try:
+        nr_videos = int(nr_videos_str)
+    except ValueError:
+        print("Invalid number of videos")
+        exit()
 
     crawler = YoutubeAPI()
+
+    start_time = time.time()
 
     crawler.search(keyword, nr_videos)
     crawler.process_results()
 
-    crawler.process_tokens(nr_videos)
+    #crawler.process_tokens(nr_videos)
+
+    end_time = time.time()
+    execution_time = time.strftime("%H:%M:%S", time.gmtime(end_time - start_time))
+    print("Execution time: " + execution_time)
