9c8c0bed3bc37acfef6f3f796b053ccfcb27f37d
alexgrigoras <alex_grigoras_10@yahoo.com>
2020-03-30 20:03:14 +0300

    Added function calls to create edgelist for users and create network data

--- application/__main__.py
+++ application/__main__.py
@@ -1,6 +1,6 @@
 import time
 from application.modules.youtube_api import YoutubeAPI
-
+from application.modules.network_analysis import NetworkAnalysis
 
 if __name__ == '__main__':
     keyword = input('Enter a keyword: ')
@@ -14,13 +14,18 @@ if __name__ == '__main__':
         exit()
 
     crawler = YoutubeAPI()
+    network = NetworkAnalysis()
 
     start_time = time.time()
 
     crawler.search(keyword, nr_videos)
     crawler.process_results()
 
-    #crawler.process_tokens(nr_videos)
+    crawler.process_tokens(nr_videos)
+
+    file_path = crawler.create_users_edge_list("\"ksCrgYQhtFrXgbHAhi9Fo5t0C2I/qAjSavC3x9fPWugAxzknLDt5TnM\"")
+
+    network.create_network("data/network_hGRqbqLDkL")
 
     end_time = time.time()
     execution_time = time.strftime("%H:%M:%S", time.gmtime(end_time - start_time))
